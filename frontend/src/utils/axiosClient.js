6e33c856952e924be0b39b98d5db9d298a12251a
Sk Abdur Rohid <abdurrohid339@gmail.com>
2025-10-20 20:04:21 +0530

    Update axiosClient.js

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,11 +1,19 @@
-import axios from "axios"
+// import axios from "axios"
 
-const axiosClient =  axios.create({
-    baseURL: 'http://localhost:9000',
-    withCredentials: true,
-    headers: {
-        'Content-Type': 'application/json'
-    }
+// const axiosClient =  axios.create({
+//     baseURL: 'http://localhost:9000',
+//     withCredentials: true,
+//     headers: {
+//         'Content-Type': 'application/json'
+//     }
+// });
+
+const axiosClient = axios.create({
+  baseURL: import.meta.env.VITE_API_URL, // 👈 loads from .env
+  withCredentials: true,                 // 👈 allow cookies/session if needed
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 
