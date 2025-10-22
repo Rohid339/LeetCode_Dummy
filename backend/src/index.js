078c8ac4c5dc7310f2e2df12294502e5383ccdbe
Sk Abdur Rohid <abdurrohid339@gmail.com>
2025-10-22 18:50:45 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,91 +1,91 @@
-// const express = require('express')
-// const app = express();
-// require('dotenv').config();
-// const main =  require('./config/db')
-// const cookieParser =  require('cookie-parser');
-// const authRouter = require("./routes/userAuth");
-// const redisClient = require('./config/redis');
-// const problemRouter = require("./routes/problemCreator");
-// const submitRouter = require("./routes/submit")
-// const aiRouter = require("./routes/aiChatting")
-// const videoRouter = require("./routes/videoCreator");
-// const cors = require('cors')
-// const PORT = process.env.PORT || 9000;
-
-// // console.log("Hello")
-
-// // app.use(cors({
-// //     origin: 'http://localhost:5173',
-// //     credentials: true 
-// // }))
-
-// const allowedOrigins = [
-//   'http://localhost:5173',
-//   // 'https://leetcode-clone-roan.vercel.app'
-// ];
-
-// app.use(cors({
-//   origin: function (origin, callback) {
-    
-//     if (!origin || allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-//   credentials: true
-// }));
-
-
-
-// app.use(express.json());
-// app.use(cookieParser());
-
-// app.use('/user',authRouter);
-// app.use('/problem',problemRouter);
-// app.use('/submission',submitRouter);
-// app.use('/ai',aiRouter);
-// app.use("/video",videoRouter);
-
-
-// const InitalizeConnection = async ()=>{
-//     try{
-
-//         await Promise.all([main(),redisClient.connect()]);
-//         console.log("DB Connected");
-        
-//         // app.listen(process.env.PORT, ()=>{
-//         //     console.log("Server listening at port number: "+ process.env.PORT);
-//         // })
-
-//       // app.listen(PORT, () => {
-//       //     console.log(`✅ Server listening on port: ${PORT}`);
-//       //   });
-
-//       app.listen(PORT, "0.0.0.0", () => {
-//           console.log(`✅ Server listening on port: ${PORT}`);
-//       });
-
-//     }
-//     catch(err){
-//         console.log("Error: "+err);
-//     }
-// }
-
-
-// InitalizeConnection();
-
-
-
-const express = require('express');
+const express = require('express')
 const app = express();
+require('dotenv').config();
+const main =  require('./config/db')
+const cookieParser =  require('cookie-parser');
+const authRouter = require("./routes/userAuth");
+const redisClient = require('./config/redis');
+const problemRouter = require("./routes/problemCreator");
+const submitRouter = require("./routes/submit")
+const aiRouter = require("./routes/aiChatting")
+const videoRouter = require("./routes/videoCreator");
+const cors = require('cors')
 const PORT = process.env.PORT || 9000;
 
-app.get('/', (req, res) => {
-  res.send('Render test success!');
-});
+// console.log("Hello")
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`✅ Listening on ${PORT}`);
-});
+// app.use(cors({
+//     origin: 'http://localhost:5173',
+//     credentials: true 
+// }))
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  // 'https://leetcode-clone-roan.vercel.app'
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
+
+
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/user',authRouter);
+app.use('/problem',problemRouter);
+app.use('/submission',submitRouter);
+app.use('/ai',aiRouter);
+app.use("/video",videoRouter);
+
+
+const InitalizeConnection = async ()=>{
+    try{
+
+        await Promise.all([main(),redisClient.connect()]);
+        console.log("DB Connected");
+        
+        // app.listen(process.env.PORT, ()=>{
+        //     console.log("Server listening at port number: "+ process.env.PORT);
+        // })
+
+      // app.listen(PORT, () => {
+      //     console.log(`✅ Server listening on port: ${PORT}`);
+      //   });
+
+      app.listen(PORT, "0.0.0.0", () => {
+          console.log(`✅ Server listening on port: ${PORT}`);
+      });
+
+    }
+    catch(err){
+        console.log("Error: "+err);
+    }
+}
+
+
+InitalizeConnection();
+
+
+
+// const express = require('express');
+// const app = express();
+// const PORT = process.env.PORT || 9000;
+
+// app.get('/', (req, res) => {
+//   res.send('Render test success!');
+// });
+
+// app.listen(PORT, "0.0.0.0", () => {
+//   console.log(`✅ Listening on ${PORT}`);
+// });
 
