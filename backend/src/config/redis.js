ea4fe3218d9b2174d1862a01299edea1b4392d49
Sk Abdur Rohid <abdurrohid339@gmail.com>
2025-10-22 18:49:07 +0530

    Update redis.js

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -4,8 +4,8 @@ const redisClient = createClient({
     username: 'default',
     password: process.env.REDIS_PASS,
     socket: {
-        host: 'redis-18691.c305.ap-south-1-1.ec2.redns.redis-cloud.com',
-        port: 18691
+        host: 'redis-10725.crce179.ap-south-1-1.ec2.redns.redis-cloud.com',
+        port: 10725
     }
 });
 
